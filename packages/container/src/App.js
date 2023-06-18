de1d5bc6cdce4d0e7ae36f7afe887f43a82787f0
andrii-kryvda <a.kryvda@spd-ukraine.com>
2023-06-18 14:08:59 +0300

    added header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
+import { BrowserRouter } from "react-router-dom";
 
 export default () => {
   console.log("Container TEST");
+
   return (
-    <div>
-      <h1>Container</h1>
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
   );
 };
