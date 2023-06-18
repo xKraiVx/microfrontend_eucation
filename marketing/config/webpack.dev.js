22c9aef6dbede9736bea8a5961abdba97a9e3bfc
andrii-kryvda <a.kryvda@spd-ukraine.com>
2023-06-18 11:44:07 +0300

    Created container deploy script

--- marketing/config/webpack.dev.js
+++ marketing/config/webpack.dev.js
@@ -1,6 +1,5 @@
 const { merge } = require("webpack-merge");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const commonConfig = require("./webpack.common");
 
 const deps = require("../package.json").dependencies;
@@ -13,9 +12,6 @@ const devConfig = {
     },
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
     new ModuleFederationPlugin({
       name: "marketing",
       filename: "remoteEntry.js",
