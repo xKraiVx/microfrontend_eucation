9c171398daf3b8bf807b7647be75d8d9ecc039b2
andrii-kryvda <a.kryvda@spd-ukraine.com>
2023-06-18 15:04:03 +0300

    fixed issue with the same generated classes to container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,16 +2,23 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 import { BrowserRouter } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 export default () => {
   console.log("Container TEST");
 
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
