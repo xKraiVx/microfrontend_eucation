1ab2b500997c3730c247395a6dfcfd6916ba2ec5
andrii-kryvda <a.kryvda@spd-ukraine.com>
2023-06-18 13:01:18 +0300

    test deploy

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 
 export default () => {
-  console.log("Container");
+  console.log("Container TEST");
   return (
     <div>
       <h1>Container</h1>
