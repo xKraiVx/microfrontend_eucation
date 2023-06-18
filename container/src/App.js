534d8f5b495d8280364c1efc5b2ee4708563b43e
andrii-kryvda <a.kryvda@spd-ukraine.com>
2023-06-18 11:58:35 +0300

    test commit

--- container/src/App.js
+++ container/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 
 export default () => {
+  console.log("Container");
   return (
     <div>
       <h1>Container</h1>
